c22fb2f8ff17d85e23f1d6ed7575535f610782be
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2023-05-07 05:09:36 +0300

    install react-scripts

--- src/App.js
+++ src/App.js
@@ -16,24 +16,24 @@ function App() {
           <Link to="/duolingo-dashboard">All</Link>
         </li>
         <li>
-          <Link to="/Study Place">Study</Link>
+          <Link to="/duolingo-dashboard/Study Place">Study</Link>
         </li>
         <li>
-          <Link to="/Neighborhood">City</Link>
+          <Link to="/duolingo-dashboard/Neighborhood">City</Link>
         </li>
         <li>
-          <Link to="/Province">Province</Link>
+          <Link to="/duolingo-dashboard/Province">Province</Link>
         </li>
         <li>
-          <Link to="/users">Users</Link>
+          <Link to="/duolingo-dashboard/users">Users</Link>
         </li>
       </ul>
       <Routes>
         <Route path="/" element={<Leaderboard />} />
         <Route path="/duolingo-dashboard" element={<Leaderboard />} />
-        <Route path="/users" element={<RankListUsers showTop3 />} />
-        <Route path="/users/:filterKey/:filterValue" element={<RankListUsers showTop3 />} />
-        <Route path="/:filter" element={<RankListItem />} />
+        <Route path="/duolingo-dashboard/users" element={<RankListUsers showTop3 />} />
+        <Route path="/duolingo-dashboard/users/:filterKey/:filterValue" element={<RankListUsers showTop3 />} />
+        <Route path="/duolingo-dashboard/:filter" element={<RankListItem />} />
       </Routes>
     </Router>
   );
